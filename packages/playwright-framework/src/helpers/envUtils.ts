269dccf130975727be210671ae9d56b0e2102aae
Netanel Harush <87550014+NetanelH2@users.noreply.github.com>
2025-09-26 10:31:07 +0300

    Add environment utilities helper

---
+++ packages/playwright-framework/src/helpers/envUtils.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config({quiet: true})
+
+export const getEnvCredentials = (credentials: string): string => {
+  if (!process.env[credentials]) {
+    throw new Error(`Environment variable ${credentials} is not set.`)
+  }
+  return process.env[credentials] as string
+}
